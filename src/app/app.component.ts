c3c0c0d199fe107d8d664f5d1f41ec19f4619136
LRLayan <rameshlayan4@gmail.com>
2024-08-05 17:27:06 +0530

    remove imort section-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from "./home/home.component";
-import { SectionEndComponent } from './section-end/section-end.component';
 import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, SectionEndComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
