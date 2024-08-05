88e131e16c519c27f6e9f1f945e5e59f514e68b4
LRLayan <rameshlayan4@gmail.com>
2024-08-05 17:27:54 +0530

    import section-end

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SectionEndComponent } from "../section-end/section-end.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [SectionEndComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
